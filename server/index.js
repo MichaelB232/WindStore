d829079da71d1235e84d2df274b3d202f68ff6fc
MichaelB232 <michaelbintang2006@gmail.com>
2026-06-06 21:05:25 +0700

    Perubahan aturan main

--- server/index.js
+++ server/index.js
@@ -6,12 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/message", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello from Express!" });
 });
-
-const PORT = 5000;
-
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
